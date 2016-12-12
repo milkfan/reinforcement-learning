71242a80a2f80a1774fb7b2c78d8256209b15b0f
Andy Mullenix <amullenix@gmail.com>
2016-12-12 15:10:24 -0600

    Use deep_rl's atari emulator.

--- DQN/dqn.py
+++ DQN/dqn.py
@@ -12,18 +12,11 @@ if "../" not in sys.path:
   sys.path.append("../")
 
 from lib import plotting
-from lib.atari.helpers import (
-    AtariEnvWrapper, atari_make_next_state, atari_make_initial_state
-)
 from lib.atari.q_network import QNetwork
 from lib.atari.memory import Memory
+from lib.atari.emulator import AtariEmulator
 from collections import deque, namedtuple
 
-
-env = AtariEnvWrapper(gym.envs.make("BreakoutDeterministic-v0"))
-
-# Atari Actions: 0 (noop), 1 (fire), 2 (left) and 3 (right) are valid actions
-VALID_ACTIONS = [0, 1, 2, 3]
 
 class StateProcessor():
     def process(self, state):
@@ -43,14 +36,6 @@ def make_epsilon_greedy_policy(q_network, num_actions):
         the probabilities for each action in the form of a numpy array of length nA.
 
     """
-    # def policy_fn(observation, epsilon, step):
-    #     A = np.ones(nA, dtype=float) * epsilon / nA
-    #     q_values = estimator.inference(np.expand_dims(observation, 0), step)[0]
-    #     best_action = np.argmax(q_values)
-    #     A[best_action] += (1.0 - epsilon)
-    #     return A
-    # return policy_fn
-
     def policy_fn(state, epsilon, step):
         if random.random() >= epsilon:
             #q_values = q_network.inference(np.expand_dims(state, 0), step)
@@ -62,7 +47,7 @@ def make_epsilon_greedy_policy(q_network, num_actions):
 
 
 def deep_q_learning(args,
-                    env,
+                    emulator,
                     q_network,
                     state_processor,
                     num_episodes,
@@ -79,7 +64,7 @@ def deep_q_learning(args,
     Finds the optimal greedy policy while following an epsilon-greedy policy.
 
     Args:
-        env: OpenAI environment
+        emulator: Wraps ALE and performs random walk in reset per DM paper
         q_network: Both target and behavior networks
         target_estimator: Estimator object used for the targets
         state_processor: A StateProcessor object
@@ -98,11 +83,8 @@ def deep_q_learning(args,
         An EpisodeStats object with two numpy arrays for episode_lengths and episode_rewards.
     """
 
-    Transition = namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
-
     # The replay memory
-    #replay_memory = []
-    memory = Memory(args, len(VALID_ACTIONS))
+    memory = Memory(args, emulator.num_actions)
 
     # Keeps track of useful statistics
     stats = plotting.EpisodeStats(
@@ -127,50 +109,44 @@ def deep_q_learning(args,
     # The policy we're following
     policy = make_epsilon_greedy_policy(
         q_network,
-        len(VALID_ACTIONS))
+        emulator.num_actions
+    )
 
     # Populate the replay memory with initial experience
     print("Populating replay memory...")
-    frame = env.reset()
-    frame = state_processor.process(frame)
-    #state = atari_make_initial_state(state)
-    memory.add(frame, 0, 0, int(False))
-    #for i in range(args.initial_memory_length):
+    initial_state = emulator.reset()
+    for experience in initial_state:
+        memory.add(experience[0], experience[1], experience[2], experience[3])
+
     for t in itertools.count():
-        #action_probs = policy(state, epsilons[total_t], total_t)
-        #action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
         state = memory.get_current_state()
         action = policy(state, epsilons[total_t], total_t)
-        one_hot_action = np.zeros(len(VALID_ACTIONS))
+        one_hot_action = np.zeros(emulator.num_actions)
         one_hot_action[action] = 1
-        next_frame, reward, done, _ = env.step(VALID_ACTIONS[action])
-        next_frame = state_processor.process(next_frame)
-        #next_state = atari_make_next_state(state, next_state)
-        #replay_memory.append(Transition(state, one_hot_action, reward, next_state, done))
-        memory.add(next_frame, action, reward, int(done))
+        state, action, reward, terminal, raw_reward = emulator.run_step(action)
+        memory.add(state, action, reward, terminal)
 
         total_t += 1
 
         # Ensure that we finish current episode once we've exceeded the initial
         # memory length
-        if t >= args.initial_memory_length and done:
+        if t >= args.initial_memory_length and emulator.isGameOver():
             break
-        elif done:
-            frame = env.reset()
-            frame = state_processor.process(frame)
-            #state = atari_make_initial_state(state)
-            memory.add(frame, 0, 0, int(False))
+        elif emulator.isGameOver():
+            initial_state = emulator.reset()
+            for experience in initial_state:
+                memory.add(experience[0], experience[1], experience[2], experience[3])
 
-    # Record videos
-    env.monitor.start(monitor_path,
-                      resume=True,
-                      video_callable=lambda count: count % record_video_every == 0)
+
+    # # Record videos
+    # env.monitor.start(monitor_path,
+    #                   resume=True,
+    #                   video_callable=lambda count: count % record_video_every == 0)
 
     # Reset the environment
-    frame = env.reset()
-    frame = state_processor.process(frame)
-    #state = atari_make_initial_state(state)
-    memory.add(frame, 0, 0, int(False))
+    initial_state = emulator.reset()
+    for experience in initial_state:
+        memory.add(experience[0], experience[1], experience[2], experience[3])
     loss = None
 
     for i_episode in range(num_episodes):
@@ -189,31 +165,17 @@ def deep_q_learning(args,
             episode_summary.value.add(simple_value=epsilon, tag="epsilon")
             q_network.summary_writer.add_summary(episode_summary, total_t)
 
-            # Print out which step we're on, useful for debugging.
             print("\rStep {} ({}) @ Episode {}/{}, loss: {}".format(
                     t, total_t, i_episode + 1, num_episodes, loss), end="")
             sys.stdout.flush()
 
             # Take a step
-            # action_probs = policy(state, epsilon, total_t)
-            # action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
             state = memory.get_current_state()
             action = policy(state, epsilon, total_t)
-            one_hot_action = np.zeros(len(VALID_ACTIONS))
+            one_hot_action = np.zeros(emulator.num_actions)
             one_hot_action[action] = 1
-            next_frame, reward, done, _ = env.step(VALID_ACTIONS[action])
-            next_frame = state_processor.process(next_frame)
-            #next_state = atari_make_next_state(state, next_state)
-
-            # # If our replay memory is full, pop the first element
-            # if len(replay_memory) == replay_memory_size:
-            #     replay_memory.pop(0)
-
-            # Save transition to replay memory
-            # replay_memory.append(
-            #     Transition(state, one_hot_action, reward, next_state, int(done))
-            # )
-            memory.add(next_frame, action, reward, int(done))
+            state, action, reward, terminal, raw_reward = emulator.run_step(action)
+            memory.add(state, action, reward, terminal)
 
             # Update statistics
             stats.episode_rewards[i_episode] += reward
@@ -221,18 +183,15 @@ def deep_q_learning(args,
 
             if total_t % train_every == 0:
                 # Sample a minibatch from the replay memory
-                #samples = random.sample(replay_memory, args.batch_size)
-                #states_batch, actions_batch, rewards_batch, next_states_batch, done_batch = map(np.array, zip(*samples))
-                states, actions, rewards, next_states, dones = memory.get_batch()
+                states, actions, rewards, next_states, terminals = memory.get_batch()
 
                 # Perform gradient descent update
-                #states_batch = np.array(states_batch)
                 loss = q_network.train(
                     states,
                     actions,
                     rewards,
                     next_states,
-                    dones
+                    terminals
                 )
 
                 if total_t % (10 * train_every) == 0:
@@ -240,20 +199,18 @@ def deep_q_learning(args,
 
             total_t += 1
 
-            if done:
+            if emulator.isGameOver():
                 try:
                     # Reset the environment
-                    frame = env.reset()
-                    frame = state_processor.process(frame)
-                    #state = atari_make_initial_state(state)
-                    memory.add(frame, 0, 0, int(False))
+                    initial_state = emulator.reset()
+                    for experience in initial_state:
+                        memory.add(experience[0], experience[1], experience[2], experience[3])
+
                     loss = None
 
                     break
                 except:
                     pass
-
-            #state = next_state
 
         # Add summaries to tensorboard
         episode_summary = tf.Summary()
@@ -266,7 +223,7 @@ def deep_q_learning(args,
             episode_lengths=stats.episode_lengths[:i_episode+1],
             episode_rewards=stats.episode_rewards[:i_episode+1])
 
-    env.monitor.close()
+    #env.monitor.close()
     return stats
 
 
@@ -274,6 +231,14 @@ def main():
     parser = argparse.ArgumentParser('a program to train or run a deep q-learning agent')
     parser.add_argument("--screen_dims", type=tuple, help="dimensions to resize frames", default=(84,84))
     parser.add_argument("--history_length", type=int, help="number of frames in a state", default=4)
+
+    parser.add_argument("--rom_path", type=str, help="path to directory containing atari game roms", default='../../roms')
+
+    parser.add_argument("--buffer_length", type=int, help="length of buffer to blend frames", default=2)
+    parser.add_argument("--blend_method", type=str, help="method used to blend frames", choices=('max'), default='max')
+    parser.add_argument("--reward_processing", type=str, help="method to process rewards", choices=('clip', 'none'), default='clip')
+    parser.add_argument("--frame_skip", type=int, help="number of frames to repeat chosen action", default=4)
+    parser.add_argument("--max_start_wait", type=int, help="max number of frames to wait for initial state", default=30)
 
     # must set network_architecture to custom in order use custom architecture
     parser.add_argument("--conv_kernel_shapes", type=tuple,
@@ -315,18 +280,20 @@ def main():
     tf.reset_default_graph()
 
     # Where we save our checkpoints and graphs
-    experiment_dir = os.path.abspath("./experiments/{}".format(env.spec.id))
+    #experiment_dir = os.path.abspath("./experiments/{}".format(env.spec.id))
+    experiment_dir = os.path.abspath("./experiments/{}".format("breakout_ale"))
 
     # Create a glboal step variable
     global_step = tf.Variable(0, name='global_step', trainable=False)
 
     state_processor = StateProcessor()
 
-    q_network = QNetwork(args, len(VALID_ACTIONS), experiment_dir)
-    memory = Memory(args, len(VALID_ACTIONS))
+    emulator = AtariEmulator(args)
+    q_network = QNetwork(args, emulator.num_actions, experiment_dir)
+    memory = Memory(args, emulator.num_actions)
 
     for t, stats in deep_q_learning(args=args,
-                                    env=env,
+                                    emulator=emulator,
                                     q_network=q_network,
                                     state_processor=state_processor,
                                     experiment_dir=experiment_dir,
